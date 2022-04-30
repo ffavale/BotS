7679fd119da257fe3fce962e59393b9f7a5061a9
Valerio Croce <valecroce.supernuke@gmail.com>
2022-04-30 20:34:11 +0200

    Mission Accomplished: Added SplashScreen Function that prints the SplashScreen (?)

--- src/bots/Main.java
+++ src/bots/Main.java
@@ -1,9 +1,36 @@
 package bots;
 
+import java.net.StandardSocketOptions;
+
 public class Main{
+
+    public static void SplashScreen(){
+        System.out.println("#####################################################################################");
+        System.out.println("/ // // // // // // // // // // // // // // // // // // // // // // // // // // // //");
+        System.out.println("#####################################################################################");
+        System.out.println("");
+        System.out.println("          ________            ____        __  __  __              ____  ");
+        System.out.println("         /_  __/ /_  ___     / __ )____ _/ /_/ /_/ /__     ____  / __/ ");
+        System.out.println("          / / / __ \\/ _ \\   / __  / __ `/ __/ __/ / _ \\   / __ \\/ /_");
+        System.out.println("         / / / / / /  __/  / /_/ / /_/ / /_/ /_/ /  __/  / /_/ / __/");
+        System.out.println("        /_/ /_/ /_/\\___/  /_____/\\__,_/\\__/\\__/_/\\___/   \\____/_/");
+        System.out.println("                __  __            _____");
+        System.out.println("               / /_/ /_  ___     / ___/___  _  _____  _____");
+        System.out.println("              / __/ __ \\/ _ \\    \\__ \\/ _ \\| |/_/ _ \\/ ___/");
+        System.out.println("             / /_/ / / /  __/   ___/ /  __/>  </  __(__  )");
+        System.out.println("             \\__/_/ /_/\\___/   /____/\\___/_/|_|\\___/____/");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("#####################################################################################");
+        System.out.println("// // // // // // // // // // // // // // // // // // // // // // // // // // // // /");
+        System.out.println("#####################################################################################");
+
+
+    }
+
 
     /* Program starts here */
     public static void main(String[] args){
-
+        SplashScreen();
     }
 }
