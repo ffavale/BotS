d78a8f18aea90a16b6a698cd23d7429214376087
Francesco Favale <francesco@favale.eu>
2022-05-04 17:37:27 +0200

    Commented for clarity

--- src/bots/simulation/Event.java
+++ src/bots/simulation/Event.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 public class Event
 {
+    // define the templates used to create events
     public static final EventTemplate[] eventTemplates = {
         new EventTemplate("Date", 2, 3, 100, 5000, 6000, 10000),
         new EventTemplate("Gathering", 3, 7, 100, 2500, 3000, 6000),
@@ -24,12 +25,15 @@ public class Event
 
     public Event(int i_iterationNumber, ArrayList<Individual> i_candidates, Logg i_log)
     {
+        // set eventId and log
         this.eventId = i_iterationNumber;
         this.log = i_log;
 
+        // define the type of event this is
         Random rng = new Random();
         EventTemplate thisEvent = Event.eventTemplates[rng.nextInt(Event.eventTemplates.length)];
 
+        // decide what its specific charachteristics are
         this.participantCount = rng.nextInt(thisEvent.maxAttendees - thisEvent.minAttendees) + thisEvent.minAttendees;
         this.minAge = rng.nextInt(thisEvent.maxMinAge - thisEvent.minMinAge) + thisEvent.minMinAge;
         this.maxAge = rng.nextInt(thisEvent.maxMaxAge - thisEvent.minMaxAge) + thisEvent.minMaxAge;
@@ -41,14 +45,17 @@ public class Event
                 " with " + this.participantCount + " participants" +
                 " between the ages of " + this.minAge + " and " + this.maxAge); //, "Event-" + String.valueOf(this.eventId));
 
+        // populate the event
         this.participants = this.selectAttendees(createInvitationList(i_candidates), rng);
     }
 
     public Event(int i_iterationNumber, ArrayList<Individual> i_candidates, Logg i_log, boolean i_isUniversal)
     {
+        // set eventId and log
         this.eventId = i_iterationNumber;
         this.log = i_log;
 
+        // decide what its specific charachteristics are
         this.participantCount = i_candidates.size();
         this.minAge = 0;
         this.maxAge = i_iterationNumber;
@@ -59,6 +66,7 @@ public class Event
                 " of type \"" + this.eventTypeName + "\"" +
                 " with " + this.participantCount + " participants"); //, "Event-" + String.valueOf(this.eventId));
 
+        // populate the event
         this.participants = new Individual[this.participantCount];
         for (int i = 0; i < i_candidates.size(); i++)
         {
@@ -70,40 +78,43 @@ public class Event
     private ArrayList<Individual> createInvitationList(ArrayList<Individual> i_candidates)
     {
         int satisfactoryCandidateCount = 0;
-        ArrayList<Individual> satisfactoryCandidates = new ArrayList<Individual>();
+        ArrayList<Individual> invitationList = new ArrayList<Individual>();
 
+        // find all suitable candidates and add them to the invitation list
         for (Individual ind : i_candidates)
         {
             if (this.minAge <= ind.getAge() && ind.getAge() <= this.maxAge)
             {
                 satisfactoryCandidateCount++;
-                satisfactoryCandidates.add(ind);
+                invitationList.add(ind);
             }
         }
 
+        // resize event if not enough suitable candidates are found
         if (satisfactoryCandidateCount < this.participantCount)
         {
             this.participantCount = satisfactoryCandidateCount;
             this.log.logMessage("Event capacity resized to " + String.valueOf(this.participantCount) + " due to lack of suitable candidates for the event", "Event-" + String.valueOf(this.eventId));
         }
 
-        return satisfactoryCandidates;
+        return invitationList ;
     }
 
     private Individual[] selectAttendees(ArrayList<Individual> i_candidates, Random i_rng)
     {
-        Individual[] returnIndividualArray = new Individual[this.participantCount];
+        Individual[] attendeeArray = new Individual[this.participantCount];
 
+        // pick attendees at random from the invitation array
         for (int i = 0; i < this.participantCount; i++)
         {
             int consideredIdx = i_rng.nextInt(i_candidates.size());
 
-            returnIndividualArray[i] = i_candidates.get(consideredIdx);
-            i_candidates.remove(returnIndividualArray[i]);
-            this.log.logMessage("Individual-" + String.valueOf(returnIndividualArray[i].getId()) + " is participating in Event-" + String.valueOf(this.eventId), "Event-" + String.valueOf(this.eventId));
+            attendeeArray[i] = i_candidates.get(consideredIdx);
+            i_candidates.remove(attendeeArray[i]);
+            this.log.logMessage("Individual-" + String.valueOf(attendeeArray[i].getId()) + " is participating in Event-" + String.valueOf(this.eventId), "Event-" + String.valueOf(this.eventId));
         }
 
-        return returnIndividualArray;
+        return attendeeArray;
     }
 }
 
