a56e6fd9fcbeac14f96a6c32e6e7cdf3cd08e1af
Francesco Favale <francesco@favale.eu>
2022-05-22 20:33:52 +0200

    Bulk changes commit

--- src/bots/simulation/Simulation.java
+++ src/bots/simulation/Simulation.java
@@ -14,11 +14,12 @@ public class Simulation extends Thread
     private Logg log;
     private ArrayList<Individual> populationArray;
 
-    private int populationCount;
     private int countF = 0;
     private int countP = 0;
     private int countC = 0;
     private int countS = 0;
+
+    private int avgAge;
 
 
     private double[] ratiosFPCS = new double[4];
@@ -26,15 +27,14 @@ public class Simulation extends Thread
 
     private long simulationSteps = 0;
 
+    private ArrayList<Snapshot> snapshotArray = new ArrayList<Snapshot>();
 
-
-    public Simulation(int popNum, double[] fpcsRatios, int[] costs)
+    public Simulation(int popNum, int avgAge, double[] fpcsRatios, int[] costs)
     {
         // give simulation its Id
         this.simID = Simulation.simCounter;
         Simulation.simCounter++;
         // assign population values
-        this.populationCount = popNum;
         this.ratiosFPCS[0] = fpcsRatios[0] * fpcsRatios[1];
         this.ratiosFPCS[1] = fpcsRatios[0] * (1 - fpcsRatios[1]);
         this.ratiosFPCS[2] = (1 - fpcsRatios[0]) * fpcsRatios[2];
@@ -42,7 +42,8 @@ public class Simulation extends Thread
         this.simulationCosts = costs;
         this.log = new Logg("Simulation-" + this.simID, "Simulation-"+ this.simID);
         // populate simulation
-        this.populationArray = populator();
+        this.avgAge = avgAge;
+        this.populationArray = populator(popNum);
         this.countF = (int) Math.round(popNum * this.ratiosFPCS[0]);
         this.countP = (int) Math.round(popNum * this.ratiosFPCS[1]);
         this.countC = (int) Math.round(popNum * this.ratiosFPCS[2]);
@@ -51,6 +52,11 @@ public class Simulation extends Thread
 
     public void info()
     {
+        double[] tempFPCS =  {
+        (double) this.countF / this.populationArray.size(),
+        (double) this.countP / this.populationArray.size(),
+        (double) this.countC / this.populationArray.size(),
+        (double) this.countS / this.populationArray.size()}; 
         String[] info = {"Faithful   ", "Philanderer", "Coy        ", "Fast       "};
         String[] bar = new String[4];
 
@@ -60,7 +66,7 @@ public class Simulation extends Thread
 
             for (int i = 1; i < 51; i++)
             {
-                if (i <= Math.round(this.ratiosFPCS[j]*50))
+                if (i <= Math.round(tempFPCS[j]*50))
                 {
                     bar[j] = bar[j] + ("/");
                 }
@@ -69,28 +75,28 @@ public class Simulation extends Thread
                     bar[j] = bar[j] + ("_");
                 }
             }
-            bar[j] = bar[j] + ("] " + Math.round(this.ratiosFPCS[j]*100) + "%\n");
+            bar[j] = bar[j] + ("] " + Math.round(tempFPCS[j]*100) + "%\n");
         }
         this.log.logMessage("\n--------------- Current Info ---------------\nPopulation: " + this.populationArray.size() + "\nRatios of FPCS:\n" + bar[0] + bar[1] + bar[2] + bar[3]);
     }
 
     public void oneLineInfo(){
         this.log.logMessage("Iteration count: " + this.simulationSteps +
-        " - Population number: " + this.populationCount +
+        " - Population number: " + this.populationArray.size() +
         " - FPCS Ratios: " + String.valueOf(Double.valueOf(this.countF) / Double.valueOf(this.populationArray.size())) +
         " " + String.valueOf(Double.valueOf(this.countP) / Double.valueOf(this.populationArray.size())) +
         " " + String.valueOf(Double.valueOf(this.countC) / Double.valueOf(this.populationArray.size())) +
         " " + String.valueOf(Double.valueOf(this.countS) / Double.valueOf(this.populationArray.size())));
     }
 
-    private ArrayList<Individual> populator()
+    private ArrayList<Individual> populator(int i_initPop)
     {
         ArrayList<Individual> res = new ArrayList<>();
         int[][] type = {{0, 0}, {0, 1}, {1, 2}, {1, 3}};
 
         for (int j = 0; j < 4; j++)
         {
-            for (int i = 0; i < Math.round(this.populationCount * this.ratiosFPCS[j]); i++)
+            for (int i = 0; i < Math.round(i_initPop * this.ratiosFPCS[j]); i++)
             {
                 res.add(new Individual(type[j][0], type[j][1], this.log));
             }
@@ -100,7 +106,7 @@ public class Simulation extends Thread
 
     public boolean isStable()
     {
-        if (this.simulationSteps < 100)
+        if (this.simulationSteps < 10)
         {
             return true;
         }
@@ -113,7 +119,7 @@ public class Simulation extends Thread
         this.log.logMessage("Simulation has started");
         this.info(); this.oneLineInfo();
         /* simulation loop goes here to be in a seperate thread */
-        while (this.isStable()) // && this.simulationSteps < 100)
+        while (this.isStable())
         {
             // create this loop's event
             Event loopEvent = new Event(this.simulationSteps, this.populationArray, this.log, true);
@@ -121,10 +127,41 @@ public class Simulation extends Thread
             ArrayList<Couple> loopCouples = loopEvent.createCouples();
             for (Couple coup : loopCouples)
             {
-                populationArray.add(coup.procreation());
+                Individual tempIndividual = coup.procreation(this.avgAge);
+                switch (tempIndividual.type)
+                {
+                    case FAITHFUL:
+                        {
+                            countF++;
+                            break;
+                        }
+                    case PHILANDERER:
+                        {
+                            countP++;
+                            break;
+                        }
+                    case COY:
+                        {
+                            countC++;
+                            break;
+                        }
+                    case FAST:
+                        {
+                            countS++;
+                            break;
+                        }
+
+                }
+                populationArray.add(tempIndividual);
             }
             // save snapshot of the simulation
-            // check stability
+            snapshotArray.add(new Snapshot(
+                        this.simulationSteps,
+                        this.populationArray.size(),
+                        (double) this.countF / this.populationArray.size(),
+                        (double) this.countP / this.populationArray.size(),
+                        (double) this.countC / this.populationArray.size(),
+                        (double) this.countS / this.populationArray.size()));
             // print info
             this.simulationSteps++;
             this.oneLineInfo();
@@ -134,17 +171,21 @@ public class Simulation extends Thread
 
     public class Snapshot
     {
+        public final long steps;
         public final int populationCount;
-        public final double ratio_FoM;
-        public final double ratio_FoP;
-        public final double ratio_CoS;
+        public final double ratio_F;
+        public final double ratio_P;
+        public final double ratio_C;
+        public final double ratio_S;
 
-        private Snapshot(int i_popCount, double i_ratio_FoM, double i_ratio_FoP, double i_ratio_CoS)
+        private Snapshot(long i_steps, int i_popCount, double i_ratio_F, double i_ratio_P, double i_ratio_C, double i_ratio_S)
         {
+            this.steps = i_steps;
             this.populationCount = i_popCount;
-            this.ratio_FoM = i_ratio_FoM;
-            this.ratio_FoP = i_ratio_FoP;
-            this.ratio_CoS = i_ratio_CoS;
+            this.ratio_F = i_ratio_F;
+            this.ratio_P = i_ratio_P;
+            this.ratio_C = i_ratio_C;
+            this.ratio_S = i_ratio_S;
         }
     }
 }
