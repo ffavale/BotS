cf05eaa45a5a0648d1ffa6b1a9e566df0047bc82
Francesco Favale <francesco@favale.eu>
2022-06-01 17:26:16 +0200

    Fixed the test build errors due to constructors but simulation

--- test/bots_test/simulation_test/TestSimulation.java
+++ test/bots_test/simulation_test/TestSimulation.java
@@ -7,7 +7,7 @@ public class TestSimulation {
     public static void populationInfoTest(){
         double[] ratio = {0.23, 0.09, 0.30, 0.38};
         int[] costs = {10, 15, 3};
-        Simulation a = new Simulation(100, ratio, costs);
+        Simulation a = new Simulation(100, 10, 100, 15, ratio, costs, false);
         a.info();
         a.oneLineInfo();
     }
